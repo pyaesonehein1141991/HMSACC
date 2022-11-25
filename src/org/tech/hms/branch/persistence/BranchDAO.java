0f01e697311d53d6dac8aa9999f1539813869cdf
DELL <pyaesonehein1141991@gmail.com>
2022-11-25 11:40:03 +0630

    create branch setup as other setup UI

--- src/org/tech/hms/branch/persistence/BranchDAO.java
+++ src/org/tech/hms/branch/persistence/BranchDAO.java
@@ -1,10 +1,45 @@
 package org.tech.hms.branch.persistence;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceException;
+import javax.persistence.TypedQuery;
+
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.tech.hms.branch.persistence.interfaces.IBranchDAO;
+import org.tech.hms.common.dto.brachDto.BranchDTO;
 import org.tech.java.component.persistence.BasicDAO;
+import org.tech.java.component.persistence.exception.DAOException;
 
 @Repository(value = "BranchDAO")
-public class BranchDAO extends BasicDAO implements IBranchDAO{
+public class BranchDAO extends BasicDAO implements IBranchDAO {
+
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public List<BranchDTO> findALLDTO() throws DAOException {
+		try {
+			TypedQuery<BranchDTO> q = em.createQuery("SELECT NEW " + BranchDTO.class.getName()
+					+ "(b.id,b.name,b.code,b.description) FROM Branch b ORDER BY b.code ASC", BranchDTO.class);
+			return q.getResultList();
+		} catch (NoResultException ne) {
+			return new ArrayList<>();
+		} catch (PersistenceException pe) {
+			throw translate("Failed to find all of Branch", pe);
+		}
+	}
+
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
+	public void deleteById(String id) throws DAOException {
+		try {
+
+		} catch (PersistenceException e) {
+			throw translate("Failed to delete branch", e);
+		}
+	}
 
 }
--- src/org/tech/hms/branch/services/interfaces/IBranchService.java
+++ src/org/tech/hms/branch/services/interfaces/IBranchService.java
@@ -1,5 +1,14 @@
 package org.tech.hms.branch.services.interfaces;
 
+import java.util.List;
+
+import org.tech.hms.common.dto.brachDto.BranchDTO;
+import org.tech.java.component.SystemException;
+
 public interface IBranchService {
+
+	List<BranchDTO> findAllBranchDto() throws SystemException;
+
+	void deleteById(String id) throws SystemException;
 
 }
